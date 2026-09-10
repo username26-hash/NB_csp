0dfd669cab11639c5a7312ecf84db1d69fd7e2c3
username26-hash <nicklolle.baron@ucas-edu.net>
2026-09-10 14:50:16 -0600

    varibles practice

--- varibles_practice.py
+++ varibles_practice.py
@@ -4,4 +4,39 @@ name = "nickolle"
 
 print(name)
 
+Number = "7"
 
+print(Number)
+
+number2 = "101"
+
+print (number2)
+
+
+breakfast = "nothing"
+
+print (breakfast)
+
+color = "grey"
+
+print (color)
+
+schoolname = "ucas"
+
+print (schoolname)
+
+year = "2026"
+
+print (year)
+
+eyecolor = "brown"
+
+print (eyecolor)
+
+myage = "14"
+
+print (myage)
+
+subject = "english"
+
+print (subject)
