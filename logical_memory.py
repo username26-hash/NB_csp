f6487f48466878007c50b625bc8bd49870bc442b
username26-hash <nicklolle.baron@ucas-edu.net>
2026-09-14 14:53:02 -0600

    memory diagram

---
+++ logical_memory.py
@@ -0,0 +1,25 @@
+# NB, logical and physical memory notes
+
+pi = 3,141592
+
+school="UCAS"
+
+print(f"the varible pi has a value of {pi} and is saved at {id(pi)}")
+
+print (f"the varible school has a value of {school} and is saved at {id(school)}") 
+
+name = "Alex"
+
+print(f"the varible name has a value of {name} and is saved at {id(name)}")
+
+age = 14
+
+print(f"the varible age has a value of {age} and is saved at {id(age)}")
+
+score = 10
+
+print(f"the varible score has a value of {score} and is saved at {id(score)}")
+
+score = 25
+
+print(f"the varible score has a value of {score} and is saved at {id(score)}")
