761d81acb5ed4ed11884aab9fd92d0bc9964f9a2
username26-hash <nicklolle.baron@ucas-edu.net>
2026-09-21 14:52:53 -0600

    silly sentences

---
+++ silly_sentences.py
@@ -0,0 +1,14 @@
+# NB p7 silly sentences 
+
+adjective= input("tell me an adjective: ").strip()
+
+nationality= input("tell me a nationality: ").strip()
+
+person= input("tell me a persons name: ").strip()
+
+noun= input("tell me a noun: ").strip()
+
+adjective= input("tell me an adjective: ").strip()
+
+noun= (" tell me a noun: ").strip()
+print ("pizza was invented by a " +adjective+ +nationality+ "chef named"+person+."to make a pizza you need to take a lump of "+noun+" and make a thin 
